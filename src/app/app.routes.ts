9c0eb24aec23f443890c965ee250cbebf7494b8b
Andressa Silva <andressa.2003abc@gmail.com>
2024-08-23 20:29:49 -0300

    Feat: rascunho final da landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { NavComponent } from './nav/nav.component';
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'nav', component: NavComponent }
 ];
