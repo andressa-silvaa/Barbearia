7fafc1139344f5fdb29f250c73de221b91833f65
Andressa Silva <andressa.2003abc@gmail.com>
2024-08-26 19:15:43 -0300

    Feat: Responsividade

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
 
