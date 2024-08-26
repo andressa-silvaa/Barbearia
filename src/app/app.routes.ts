5db1777fad9338ce7300106c44ce926669a29225
Andressa Silva <andressa.2003abc@gmail.com>
2024-08-26 19:30:05 -0300

    Fix: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'nav', component: NavComponent }
 ];
