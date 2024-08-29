eecad6f1c942a47a96a4315f0ad45fd60bcdc846
Andressa Silva <andressa.2003abc@gmail.com>
2024-08-28 22:36:32 -0300

    Feat: teste em home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
