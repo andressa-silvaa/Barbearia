ab9a1f9f2b4e5fb2dfe6192ba2186bf28d692e30
Andressa Silva <andressa.2003abc@gmail.com>
2024-09-29 21:33:32 -0300

    Fix: interação do menu

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
+  imports: [RouterModule, CommonModule]
 })
 export class NavComponent {
+  activeLink: string = 'sobre';
+  linkClicado: boolean = false;
+  constructor(private router: Router) {
 
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          this.setActiveLink(tree.fragment);
+          const element = document.getElementById(tree.fragment);
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+          }
+        }
+      }
+    });
+  }
+
+  setActiveLink(fragment: string) {
+    this.activeLink = fragment;
+    this.linkClicado = false;
+  }
+  abrirMenu(){
+    this.linkClicado = true;
+  }
 }
